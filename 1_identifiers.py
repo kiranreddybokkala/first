9124afcee147e58fc8c002d7964b5f273d9bb77d
kiranreddybokkala <165977135+kiranreddybokkala@users.noreply.github.com>
2024-04-03 22:49:48 -0400

    Add files via upload

---
+++ 1_identifiers.py
@@ -0,0 +1,22 @@
+print('hello world is mandatory fun')
+# identifiers
+'''
+a - z A - Z 0-9 are allowed and _
+must start with a-z or A-Z or _
+_ starts means private double underscore(__) means more private
+start and ends with double underscore are language specified identifiers
+reserved words are not allowed'''
+
+a = 10
+b12 = 2
+_abc = 'kiran\hello\
+reddy'
+print("this is a : ", a ,"and this b12: ", b12, "and this is _abc:", _abc);
+
+'''
+below identifiers are not allowed 
+123abc = 10
+123 = 2
+if = 1
+def = 2
+'''
